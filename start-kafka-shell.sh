a120fccca711d501c9ea85dbb03b73b1b91963a9
Aldwin Barredo <aldwin_barredo@condenast.com>
2016-06-30 14:57:56 -0400

    Added kakfa shell container to the kafkadocker_default network.

--- start-kafka-shell.sh
+++ start-kafka-shell.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -e HOST_IP=$1 -e ZK=$2 -i -t wurstmeister/kafka /bin/bash
+docker run --rm \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v $(pwd):/usr/src/app \
+-w /usr/src/app \
+-e HOST_IP=$1 \
+-e ZK=$2 \
+--net kafkadocker_default \
+-i -t wurstmeister/kafka /bin/bash
